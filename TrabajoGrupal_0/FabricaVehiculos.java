59e1a0dc40d58ed51191d861b0ac512413d4ecfd
Toewqr <teoarandalautaro@gmail.com>
2025-08-22 14:36:55 -0300

    agrego fabricas de vehiculos

--- TrabajoGrupal_0/FabricaVehiculos.java
+++ TrabajoGrupal_0/FabricaVehiculos.java
@@ -1,5 +1,11 @@
 package TrabajoGrupal_0;
 
 public abstract class FabricaVehiculos  {
+
+    public abstract void crearCarroceria();
+    public abstract void crearMotor();
+    public abstract void ensamblar();
+    
+    
     
 }
